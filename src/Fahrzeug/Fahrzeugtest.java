db4094a81e34efd92f1998a032c9d874cdf9c92d
JHoppe <sgs3.iloveit@gmail.com>
2023-10-19 12:46:49 +0200

    added junit5

--- src/Fahrzeug/Fahrzeugtest.java
+++ src/Fahrzeug/Fahrzeugtest.java
@@ -1,18 +1,32 @@
 package Fahrzeug;
+import  org.junit.jupiter.params.ParameterizedTest;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Fahrzeugtest
 {
     public static void main(String[] args)
     {
-
         Fahrzeughalter peter = new Firma("Peter");
-        Fahrzeughalter hansPeter = new NatürlichePerson("Hans","Peter");
-        PKW p1 = new PKW(peter,4,5);
+        Fahrzeughalter hansPeter = new NatürlichePerson("Hans", "Peter");
+        PKW p1 = new PKW(peter, 4, 5);
         Motorrad m1 = new Motorrad(hansPeter);
         System.out.println(p1.toString());
         System.out.println(peter.toString());
         System.out.println();
         System.out.println(hansPeter.toString());
+
+
+    }
+
+    @Test
+
+    public static void test()
+    {
 
 
     }
